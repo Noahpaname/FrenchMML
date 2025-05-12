91eb47db3304a9e838a3ae7fe8dc4c5fd9958bdf
Noahpaname <134812936+Noahpaname@users.noreply.github.com>
2025-05-12 14:37:23 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -84,7 +84,7 @@ export default {
                     </div>
                     <div class="og">
                         <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a>. Edited By <a href="https://www.youtube.com/@Noahpaname_FR">Renoah</a></p>
-                        <img src="https://media1.tenor.com/m/RaMKetcZVCAAAAAC/fnaf-2-puppet-gangnam-style.gifS"></img>
+                        <img src="https://media1.tenor.com/m/RaMKetcZVCAAAAAC/fnaf-2-puppet-gangnam-style.gifS" width=72 height=72></img>
                     </div>
                     <template v-if="editors">
                         <h3>List Editors</h3>
