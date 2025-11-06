149354f5406d7694deece172c721d542a40deac6
Noah <134812936+Noahpaname@users.noreply.github.com>
2025-11-06 17:49:11 +0100

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,10 +2,12 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import Unlimited from "./pages/Unlimited.js";
+import ULeaderboard from "./pages/ULeaderboard.js";
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/unlimited', component: Unlimited },
+    { path: '/uleaderboard', component: ULeaderboard },
 ];
