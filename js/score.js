b4e4807692cb0fcf0f56c06a7f85d78bed433329
Ytorad <guillaumecollard27@gmail.com>
2026-04-25 16:00:27 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (500 - Math.log(rank*0.6)*109) *
+    let score = (445.3 - Math.log(rank*0.6)*98) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
